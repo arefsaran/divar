9e17d7828d75aa67009be08364238142ee071072
arefsaran <34871233+arefsaran@users.noreply.github.com>
2023-12-23 04:04:19 +0330

    config swagger

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
+const SwaggerConfig = require("./src/config/swagger.config");
 
 async function main() {
     const app = express();
     const port = process.env.PORT;
-    require("./src/common/mongoose.config");
+    require("./src/config/mongoose.config");
+    SwaggerConfig(app);
     app.listen(port, () => {
-        console.log(`The server runs on port ${port}`);
+        console.log(`The server runs on http://localhost:${port}`);
     });
 }
 main();
---
+++ src/config/swagger.config.js
@@ -0,0 +1,21 @@
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+function swaggerConfig(app) {
+    const swaggerDocument = swaggerJsDoc({
+        swaggerDefinition: {
+            info: {
+                title: "divar",
+                description:
+                    "A Fullstack project like https://divar.ir/ is being developed.",
+                version: "1.0.0",
+            },
+        },
+        apis: [],
+    });
+
+    const swagger = swaggerUi.setup(swaggerDocument, {});
+    app.use("/", swaggerUi.serve, swagger);
+}
+
+module.exports = swaggerConfig;
