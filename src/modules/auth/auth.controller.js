89100a6449548adf15b334468c0a642ce97323f2
arefsaran <34871233+arefsaran@users.noreply.github.com>
2023-12-24 07:48:27 +0330

    auth/send-opt concept

---
+++ src/modules/auth/auth.controller.js
@@ -0,0 +1,30 @@
+const authService = require("./auth.service");
+const autoBind = require("auto-bind");
+const AuthMessage = require("./auth.messages");
+
+class AuthController {
+    #service;
+    constructor() {
+        autoBind(this);
+        this.#service = authService;
+    }
+    async sendOTP(req, res, next) {
+        try {
+            const { mobile } = req.body;
+            await this.#service.sendOTP(mobile);
+            return {
+                message: AuthMessage.SendOtpSuccessfully,
+            };
+        } catch (error) {
+            next(error);
+        }
+    }
+    async checkOTP(req, res, next) {
+        try {
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
+module.exports = new AuthController();
---
+++ src/modules/auth/auth.messages.js
@@ -0,0 +1,7 @@
+const AuthMessage = {
+    SendOtpSuccessfully: "Sent OTP successfully",
+    NotFound: "User not found",
+    OtpCodeNotExpired: "OTP Code not expired please try later",
+};
+
+module.exports = AuthMessage;
